c9a5c0569ce84133b28a3470b3bcb0aaf18a0120
SissiXXXY <sissixy717@gmail.com>
2020-06-10 17:17:01 +0800

    first version of multiprocessing client

--- echo-client.py
+++ echo-client.py
@@ -1,15 +1,30 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    while True:
-        user_input = input()
-        if user_input != 'finish':
-            s.sendall(user_input.encode('utf-8'))
-        else:
-            break
+
+
+    def sendmsg():
+        while True:
+            user_input = input()
+            if user_input != 'finish':
+                s.sendall(user_input.encode('utf-8'))
+            else:
+                break
+
+
+    def getrps():
         data = s.recv(1024)
         print('received', data.decode("utf-8"))
+
+thread_sendmsg = threading.Thread(target=sendmsg(), args=())
+thread_getrps = threading.Thread(target=getrps(), args=())
+p = [thread_sendmsg, thread_getrps]
+for i in range(len(p)):
+    p[i].start()
+for i in range(len(p)):
+    p[i].join()
