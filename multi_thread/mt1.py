08c734a081217a8dbd78a2f932df75bb1fa073ac
SissiXXXY <sissixy717@gmail.com>
2020-06-15 16:36:15 +0800

    create class Customer, changes on producer(add produce and producevip)

--- multi_thread/mt1.py
+++ multi_thread/mt1.py
@@ -1,20 +1,37 @@
 # -*- coding: utf-8 -*-
+import string
 import threading
 import time
 from threading import Thread
 import random
 
-num = 0
-
 
 def producer(id: str):
-    global num
     print(f"producer {id} started")
     while True:
-        # call push()
-        example.push(num)
-        num = num + 1
+        produce()
+        producevip()
         time.sleep(random.random())
+
+
+def produce():
+    localtime = time.localtime(time.time())
+    ran_str = ''.join(random.sample(string.ascii_letters, 4))
+    num = 10000
+    common = Customer(num, ran_str, 'common', localtime)
+    example.push(common)
+    num = num + 1
+    time.sleep(random.random())
+
+
+def producevip():
+    localtime = time.localtime(time.time())
+    ran_str = ''.join(random.sample(string.ascii_letters, 4))
+    numvip = 999000
+    vip = Customer(numvip, ran_str, 'vip', localtime)
+    example.push(vip)
+    numvip = numvip + 1
+    time.sleep(random.random() * 2)
 
 
 def consumer(id: str):
@@ -26,9 +43,18 @@ def consumer(id: str):
         work(id, x)
 
 
-def work(id, cus):
-    print(f"consumer {id} now serving customer: {cus}")
+def work(id, customer):
+    print(f"consumer {id} now serving customer number: {customer.number}, type: {customer.type}")
     time.sleep(random.random() * 3)
+
+
+class Customer:
+    def __init__(self, number, name, type, arrival_time):
+        self.number = number
+        self.name = name
+        self.type = type
+        self.arrival_time = arrival_time
+        self.lock = threading.Lock()
 
 
 class SimpleQueue:
@@ -68,13 +94,13 @@ if __name__ == '__main__':
     example = SimpleQueue()
 
     # start consumers
-    for i in range(15):
+    for i in range(2):
         consumer_thread = Thread(target=consumer, args=(f"C{i + 1}",))
         consumer_thread.start()
         """consumer_thread.join()"""
 
     # start producers
-    for i in range(5):
+    for i in range(3):
         producer_thread = Thread(target=producer, args=(f"P{i + 1}",))
         producer_thread.start()
         """producer_thread.join()"""
