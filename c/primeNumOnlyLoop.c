44ad1c5908cb27517b3a9ab168c72a57d846c6d8
SissiXXXY <sissixy717@gmail.com>
2020-06-19 16:37:05 +0800

    code update

--- c/primeNumOnlyLoop.c
+++ c/primeNumOnlyLoop.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int isPrime(num) {
-	int i;
+int isPrime(int num) {
 	int flag = 1;
-	for (i = 2; i <= num - 1; i++) {
+	for (int i = 2; i <= num - 1; i++) {
 		if (num % i == 0) {
 			flag = 0;
 			break;
@@ -13,12 +12,12 @@ int isPrime(num) {
 }
 
 int main(void) {
-	int k;
-	int sum;
-	for (k = 2; k <= 1000; k++) {
+	int sum=0;
+	for (int k = 2; k <= 1000; k++) {
 		if (isPrime(k)) {
 			sum += k;
 		}
 	}
 	printf("the sum is: %d \n", sum);
+	return 0;
 }
