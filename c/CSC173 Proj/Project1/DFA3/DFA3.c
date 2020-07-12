25363ffc61b29631e7039652a4c8ef3fdfe31463
SissiXXXY <sissixy717@gmail.com>
2020-07-12 23:43:25 +0800

    malloc prob

---
+++ c/CSC173 Proj/Project1/DFA3/DFA3.c
@@ -0,0 +1,75 @@
+#include <stdio.h> 
+#include <string.h>
+#define TRUE 1
+#define FALSE 0
+
+
+typedef struct {
+	int current_status;//0: start, even 0s,  1: odd 0s,  2: final
+	char input;
+	int output; // -1: void,  0: false,  1: true
+	int next_status;
+}DFA_Row;
+
+int row_init(DFA_Row* prow, int current_status, char input, int output, int next_status)
+{
+	DFA_Row this = (DFA_Row)malloc(sizeof(DFA_Row));
+	prow->current_status = current_status;
+	prow->input = input;
+	prow->output = output;
+	return prow->current_status != NULL;
+}
+
+DFA_Row DFA3_table[] = {
+	{0, '0', -1, 1}, //
+	{0, '1', -1, 0},
+	{0, '\0', 1, 2},
+	{1, '0', -1, 0},
+	{1, '1', -1, 1},
+	{1, '\0', 0, 2}
+
+};
+
+int transit(int current_status, char input, int* output, int* next_status, int row)
+{	
+	int result = -1;
+	DFA_Row* dfar=DFA3_table;
+	current_status = 0;
+	for (int i = 0; i < row; i++) {
+		if (current_status == dfar->current_status) {//first element match
+			if (strcmp(input, dfar->input) == 0) {//second element match
+				//get the output and next status
+				next_status = dfar->next_status;
+				output = dfar->output;
+				return TRUE;//successfully get identical row
+
+				if (next_status == 2) {
+					break;//final status
+				}
+
+			}
+		}
+	}
+	return FALSE;//failed to get identical row
+
+}
+
+
+
+void main() {
+	char* input_string = "01010001010100101110100101";
+	int row_count = sizeof(DFA3_table) / sizeof(DFA_Row);
+	int current = 0;
+	int nextsta = 0;
+	int output = -1;
+	int* outp = &output;
+	int* next = &nextsta;
+	/// <summary>
+	/// initialize the table here?
+	/// </summary>
+	for (int i = 0; i < strlen(input_string); i++) {
+		current = nextsta;
+		char inp = input_string[i];
+		transit(current, inp, outp, next, row_count);
+	}
+}
