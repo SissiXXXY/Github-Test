2101ea0c04bf208b4e72cf3e139ad525e4932adb
chenjn <chenjn@zotac>
2020-07-13 16:08:28 +0800

    code review

--- c/CSC173 Proj/Project1/DFA3/DFA3.c
+++ c/CSC173 Proj/Project1/DFA3/DFA3.c
@@ -1,74 +1,79 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 #define TRUE 1
 #define FALSE 0
 
 
-typedef struct {
-	int current_status;//0: start, even 0s,  1: odd 0s,  2: final
-	char input;
-	int output; // -1: void,  0: false,  1: true
-	int next_status;
-}DFA_Row;
+typedef struct
+{
+    int current_status; //0: start, even 0s,  1: odd 0s,  2: final
+    char input;
+    int output; // -1: void,  0: false,  1: true
+    int next_status;
+} DFA_Row;
 
 DFA_Row DFA3_table[] = {
-	{0, '0', -1, 1}, //
-	{0, '1', -1, 0},
-	{0, '\0', 1, 2},
-	{1, '0', -1, 0},
-	{1, '1', -1, 1},
-	{1, '\0', 0, 2}
+    {0, '0', -1, 1}, //
+    {0, '1', -1, 0},
+    {0, '\0', 1, 2},
+    {1, '0', -1, 0},
+    {1, '1', -1, 1},
+    {1, '\0', 0, 2}
 
 };
 
-int transit(int current_status, char input, int* output, int* next_status, int row)
-{	
-	int result = -1;
-	DFA_Row* dfar=DFA3_table;
-	current_status = 0;
-	for (int i = 0; i < row; i++) {
-		dfar = &DFA3_table[i];
-		if (current_status == dfar->current_status) {//first element match
-			if (input== dfar->input) {//second element match
-				//get the output and next status
-				*next_status = dfar->next_status;
-				*output = dfar->output;
-				return TRUE;//successfully get identical row
+int transit(const DFA_Row* const dfa_tbl, const int dfa_row_cnt,
+            const int current_status, const char input,
+            int* const output, int* const next_status)
+{
+    for (int i = 0; i < dfa_row_cnt; i++)
+    {
+        DFA_Row* row = &DFA3_table[i];
+        if (current_status == row->current_status && input == row->input)
+        {
+            //get the output and next status
+            *next_status = row->next_status;
+            *output = row->output;
+            printf("(%d, %d) --> (%d,%d)\n", current_status, input, *next_status, *output);
+            return TRUE; //successfully get identical row
+        }
+    }
 
-				if (*next_status == 2) {
-					break;//final status
-				}
-
-			}
-		}
-	}
-	return FALSE;//failed to get identical row
-
+    printf("ERROR: (%d, %d) no next status\n", current_status, input);
+    return FALSE; //failed to get identical row
 }
 
+void main()
+{
+    const char* input_string = "10101010001110101010101";
+    const int row_count = sizeof(DFA3_table) / sizeof(DFA_Row);
+    int current = 0;
+    int nextsta = 0;
+    int output = 0;
 
+    char* input_char_ptr = (char*)input_string;
+    while (1)
+    {
+        const int ret = transit(DFA3_table, row_count, current, *input_char_ptr, &output, &nextsta);
+        if (ret == FALSE)
+        {
+            printf("failed, unexpected internal error\n");
+            return;
+        }
 
-void main() {
-	char* input_string = "01010001010100101110100101";
-	int row_count = sizeof(DFA3_table) / sizeof(DFA_Row);
-	int current = 0;
-	int nextsta = 0;
-	int output = -1;
-	int* outp = &output;
-	int* next = &nextsta;
+        if (nextsta == 2)
+        {
+            printf("reach final status. output = %d\n", output);
+            break;
+        }
 
-	for (int i = 0; i < (int)strlen(input_string); i++) {
-		current = nextsta;
-		char inp = input_string[i];
-		transit(current, inp, outp, next, row_count);
-	}
-	if (*outp == 1) {
-		printf("TRUE");
-	}
-	else if (*outp == 0) {
-		printf("FALSE");
-	}
-	else {
-		printf("FAILED");
-	}
+        if (*input_char_ptr == '\0')
+        {
+            printf("input string finished. current status=%d\n", nextsta);
+            break;
+        }
+
+        input_char_ptr++;
+        current = nextsta;
+    }
 }
